8452e4d983af9746a1ec72e2229f2d2743e75ac2
Tony Gray <tony_gray@paralynx.com>
2017-09-23 13:35:15 -0700

    Adding rudimentary logging and retries for both serial read and thingspeak write, to ride through device driver delays during startup and also network dropouts. Note that during retries for a network dropout, we don't keep reading the serial port, so they will just buffer up there and when network is reestablished, it will look like all the stacked up roadings happened in quick succession. Would be better to keep the serial reading going, saving the readings with the timestamps in a structure waiting for network comms to be reestablished.  Actually, since the network comms are just strings, could do this by writing the ready-to-go strings to a file and then just looping through that file. Tony Gray

--- Dylos2Thingspeak.py
+++ Dylos2Thingspeak.py
@@ -1,19 +1,35 @@
 #!/usr/bin/env python
   
-
 import time
+import datetime
 import serial
 import requests
 import sys
 
 if len(sys.argv) < 2:
-	print "Dylos2Thingspeak accepts a single parameter, the API write key for your "
+	print "Dylos2Thingspeak requires a single parameter, the API write key for your "
 	print "Thingspeak channel as described at"
 	print "https://www.mathworks.com/help/thingspeak/update-channel-feed.html"
+	print "and an optional parameter 'basefield=' which is the number of the "
+	print "field to write the small particle count to."
 	exit()
 
+logfilename = "Dylos2Thingspeak.log"
+def LogString(outstring):
+	print(outstring)
+	f = open (logfilename, 'a') 
+	f.write ('%s %s\n' % (datetime.datetime.now().isoformat(), outstring))
+	f.close
+
 apikey = sys.argv[1]
-print "Using api key: ", sys.argv[1]
+LogString("Using api key: " + sys.argv[1])
+if(len(sys.argv) == 3):
+	basefield = int(sys.argv[2])
+	small_particle_field = "field%s" % (basefield)
+	large_particle_field = "field%s" % (basefield+1)
+else:
+	small_particle_field = "field1"
+	large_particle_field = "field2"
 
 ser = serial.Serial(
 	port='/dev/ttyUSB0',
@@ -25,16 +41,50 @@ ser = serial.Serial(
 )
 
 thingurl='https://api.thingspeak.com/update'
+#datetime.datetime.now() to get the current timestamp
+#add .isoformat() to get a string
+
+DylosSerialInput = ""
+retries = 1000
 
 while 1:
-	x=ser.readline()
-	print( x[:len(x)-2])
-	y = x[:len(x)-2].split(",")
-	print( "Small Particles: " , y[0])
-	print( "Large Particles: " , y[1])
-	payload = { 'api_key': apikey, 'field1': y[0], 'field2': y[1] }
-	r=requests.get(thingurl,params=payload)
-	print(r.url)
-	print(r)
+	while retries > 0:
+	    try:
+		DylosSerialInput=ser.readline()
+		retries = 10000
+		break # exit retry loop
+	    except Exception:
+		LogString("Reading from port %s failed, waiting 1 minute and retrying" % ("ttyUSB0"))
+		readfail = True
+		time.sleep(60)
+		retries = retries -1
 
+	if(retries == 0):
+	    LogString("Too many retries on serial, quitting")
+	    exit()
+	else:
+	    retries = 10000
+	    
+	print("From dylos:" + DylosSerialInput)
+	y = DylosSerialInput[:len(DylosSerialInput)-2].split(",")
+	outstring = "Read Small Particles: %s, Large Particles %s" % ( y[0],y[1])
+	LogString(outstring)
 
+	payload = { 'api_key': apikey, small_particle_field : y[0], large_particle_field: y[1] }
+	while retries > 0:
+	    try:
+		r=requests.get(thingurl,params=payload)
+		retries = 10000
+		outstring = "Sent to %s, response %s" % (r.url, r)
+		LogString(outstring)
+		break # exit retry loop
+	    except Exception:
+		LogString("Writing to url %s failed, waiting 1 minute and retrying" % (thingurl))
+		time.sleep(60)
+		retries = retries -1
+
+	if(retries == 0):
+	    LogString("Too many retries on serial, quitting")
+	    exit()
+	else:
+	    retries = 10000
